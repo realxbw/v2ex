24fc9887bf08b986deedd52f725e6ed8ff04fbde
¤ª¤¦ <15621624+xbw@users.noreply.github.com>
2023-07-19 15:37:53 +0800

    upgrade python to 3+

--- v2ex_daily.py
+++ v2ex_daily.py
@@ -3,8 +3,8 @@
 
 import sys
 import re
-import cookielib
-import urllib2
+from http import cookiejar
+from urllib import request
 
 # your v2ex cookie value for key [auth] after login
 # refer README.md if cannot find cookie [auth]
@@ -15,16 +15,18 @@ V2EX_URL_START = r'https://' + V2EX_DOMAIN
 V2EX_MISSION = V2EX_URL_START + r'/mission/daily'
 V2EX_COIN_URL = r'/mission/daily/redeem?once='
 
+
 def get_once_url(data):
-    p = '/mission/daily/redeem\?once=\d+'
-    m = re.search(p, data)
+    p = '/mission/daily/redeem\\?once=\\d+'
+    m = re.search(p, data.decode())
     if m:
         return m.group()
     else:
         return None
-        
+
+
 def make_cookie(name, value):
-    return cookielib.Cookie(
+    return cookiejar.Cookie(
         version=0,
         name=name,
         value=value,
@@ -42,25 +44,26 @@ def make_cookie(name, value):
         comment_url=None,
         rest=None
     )
-    
+
+
 if __name__ == '__main__':
-    cj = cookielib.CookieJar()
+    cj = cookiejar.CookieJar()
     cj.set_cookie(make_cookie('auth', V2EX_COOKIE))
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    opener = request.build_opener(request.HTTPCookieProcessor(cj))
 
     opener.addheaders = [
         ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0'),
         ('Referer', V2EX_MISSION)
     ]
-    
+
     opener.open(V2EX_URL_START).read()
-    
+
     data = opener.open(V2EX_MISSION).read()
     once = get_once_url(data)
-    if None == once:
-        print '"once" not found, maybe you already got coins'
+    if once is None:
+        print('"once" not found, maybe you already got coins')
         sys.exit(-1)
-    
-    v2ex_coin_url = V2EX_URL_START + once
-    print v2ex_coin_url
+
+    v2ex_coin_url = f'{V2EX_URL_START}{once}'
+    print(v2ex_coin_url)
     opener.open(v2ex_coin_url).read()
